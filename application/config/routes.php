86bfd8b645fdb1df45243a2da7f3e128be5dd4a1
Fathan Satria Anandika <fathansatriaanandika@gmail.com>
2019-11-14 22:45:59 +0700

    delete sb admin

--- application/config/routes.php
+++ application/config/routes.php
@@ -54,7 +54,6 @@ $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 $route['admin'] = 'admin/overview';
 $route['signin'] = 'sign_in/overview';
-
 $route['signup'] = 'sign_up/overview';//
 $route['main'] = 'main/overview'; //
 $route['search'] = 'search/overview';//
