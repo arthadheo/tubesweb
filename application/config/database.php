074519bb7e4105c1e9a861bab6dd13ef8f8c31bb
M Ariq <arqrfly@gmail.com>
2019-11-24 19:01:13 +0700

    TAMPILIN FOTO DI MAIN PAGE

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
+	'hostname' => '192.168.1.19',
+	'username' => 'user',
 	'password' => '',
-	'database' => '',
+	'database' => 'groceria',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
