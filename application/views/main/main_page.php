8f39b4b06897469e95426967cc1b23f43e21c4ac
Fathan Satria Anandika <fathansatriaanandika@gmail.com>
2019-11-24 18:51:55 +0700

    hapus comand

--- application/views/main/main_page.php
+++ application/views/main/main_page.php
@@ -33,38 +33,6 @@
                     </div>');    
                     }?>
                     
-                    <!-- <div class="card" style="margin-right: 5px;margin-top: 5 px;margin-bottom: 5 px;margin-left: 5 px;"><img class="card-img-top w-100 d-block" src="assets/img/persegi.png">
-                        <div class="card-body" style="height: 250px;">
-                            <h4 class="card-title">Daging Sapi</h4>
-                            <p class="card-text">Rp. 10.000</p><button class="btn btn-primary text-center border rounded-circle" type="button" style="background-color: #ea4335;width: 36px;height: 36px;">+</button><button class="btn btn-primary text-center border rounded-circle"
-                                type="button" style="background-color: #ea4335;width: 36px;height: 36px;">-</button>
-                            <p class="text-right card-text">3</p>
-                        </div>
-                    </div>
-                    <div class="card" style="margin-right: 5px;margin-top: 5 px;margin-bottom: 5 px;margin-left: 5 px;"><img class="card-img-top w-100 d-block" src="assets/img/persegi.png">
-                        <div class="card-body" style="height: 250px;">
-                            <h4 class="card-title">Daging Sapi</h4>
-                            <p class="card-text">Rp. 10.000</p><button class="btn btn-primary text-center border rounded-circle" type="button" style="background-color: #ea4335;width: 36px;height: 36px;">+</button><button class="btn btn-primary text-center border rounded-circle"
-                                type="button" style="background-color: #ea4335;width: 36px;height: 36px;">-</button>
-                            <p class="text-right card-text">3</p>
-                        </div>
-                    </div>
-                    <div class="card" style="margin-right: 5px;margin-top: 5 px;margin-bottom: 5 px;margin-left: 5 px;"><img class="card-img-top w-100 d-block" src="assets/img/persegi.png">
-                        <div class="card-body" style="height: 250px;">
-                            <h4 class="card-title">Daging Sapi</h4>
-                            <p class="card-text">Rp. 10.000</p><button class="btn btn-primary text-center border rounded-circle" type="button" style="background-color: #ea4335;width: 36px;height: 36px;">+</button><button class="btn btn-primary text-center border rounded-circle"
-                                type="button" style="background-color: #ea4335;width: 36px;height: 36px;">-</button>
-                            <p class="text-right card-text">3</p>
-                        </div>
-                    </div>
-                    <div class="card" style="margin-right: 5px;margin-top: 5 px;margin-bottom: 5 px;margin-left: 5 px;"><img class="card-img-top w-100 d-block" src="assets/img/persegi.png">
-                        <div class="card-body" style="height: 250px;">
-                            <h4 class="card-title">Daging Sapi</h4>
-                            <p class="card-text">Rp. 10.000</p><button class="btn btn-primary text-center border rounded-circle" type="button" style="background-color: #ea4335;width: 36px;height: 36px;">+</button><button class="btn btn-primary text-center border rounded-circle"
-                                type="button" style="background-color: #ea4335;width: 36px;height: 36px;">-</button>
-                            <p class="text-right card-text">3</p>
-                        </div>
-                    </div> -->
                 </div>
             </div>
         </div>
