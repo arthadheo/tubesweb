bd4c19f17fb96151e805533fad069bcb308d66a6
muhsanmaulana <muhsan.maulana@gmail.com>
2019-11-30 23:41:53 +0700

    checout layout

---
+++ application/views/main/checkout.php
@@ -0,0 +1,71 @@
+<div class="container text-center" style="width: 100%;height: 60px;">
+        <h2>Selesaikan orderanmu&nbsp;</h2>
+    </div>
+    <div style="width: 100%;">
+        <div class="container" style="width: 100%;padding-right: 0px;padding-bottom: 30px;">
+            <div class="row" style="background-color: #f9f9f9;width: 100%;padding-bottom: 30px;padding-top: 30px;">
+                <div class="col-md-6" style="width: 100%;">
+                    <h6>Kirim Ke</h6>
+                    <div class="dropdown" style="width: 200px;"><button class="btn btn-primary dropdown-toggle text-left" data-toggle="dropdown" aria-expanded="false" type="button" style="width: 200px;color: rgb(0,0,0);background-color: rgb(247,247,247);">Jakarta Barat</button>
+                        <div class="dropdown-menu dropdown-menu-left"
+                            role="menu" style="width: 200px;"><a class="dropdown-item" role="presentation" href="#">Jakarta Selatan</a><a class="dropdown-item" role="presentation" href="#">Jakarta Utara</a><a class="dropdown-item" role="presentation" href="#">Jakarta Timur</a></div>
+                    </div>
+                    <h6 style="margin-top: 20px;">Alamt Lengkap</h6><input class="border rounded" type="text" style="width: 400px;height: 38px;">
+                    <h6 style="margin-top: 20px;">Pilih Kurir Pengiriman</h6>
+                    <div class="form-check"><input class="form-check-input" type="radio" id="formCheck-1"><label class="form-check-label" for="formCheck-1">UMX Shipping</label></div>
+                    <div class="form-check"><input class="form-check-input" type="radio" id="formCheck-1"><label class="form-check-label" for="formCheck-1">UMX Shipping</label></div>
+                    <div class="form-check"><input class="form-check-input" type="radio" id="formCheck-1"><label class="form-check-label" for="formCheck-1">UMX Shipping</label></div>
+                </div>
+                <div class="col">
+                    <h5 style="margin-bottom: 0px;">Summary Order</h5>
+                    <p>Total biaya diperoleh dari total pembelian barang dan biaya pengirimian</p>
+                    <div style="width: 100%;">
+                        <div class="row" style="height: 20px;">
+                            <div class="col d-block" style="width: 45%;">
+                                <h6>Pengiriman</h6>
+                            </div>
+                            <div class="col d-inline-block" style="width: 45%;">
+                                <h6>Rp 20.000</h6>
+                            </div>
+                        </div>
+                        <div class="row" style="height: 20px;margin-top: 5px;">
+                            <div class="col d-block" style="width: 45%;">
+                                <h6>Pengiriman</h6>
+                            </div>
+                            <div class="col d-inline-block" style="width: 45%;">
+                                <h6>Rp 20.000</h6>
+                            </div>
+                        </div>
+                        <div class="row" style="height: 20px;margin-top: 30px;">
+                            <div class="col d-block" style="width: 45%;">
+                                <h6>Total Biaya</h6>
+                            </div>
+                            <div class="col d-inline-block" style="width: 45%;">
+                                <h6>Rp 20.000</h6>
+                            </div>
+                        </div>
+                        <h5 style="margin-top: 5px;">Bank Pembayaran</h5>
+                        <div class="dropdown"><button class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-expanded="false" type="button" style="background-color: rgb(247,247,247);color: rgb(0,0,0);">Dropdown </button>
+                            <div class="dropdown-menu" role="menu"><a class="dropdown-item" role="presentation" href="#">First Item</a><a class="dropdown-item" role="presentation" href="#">Second Item</a><a class="dropdown-item" role="presentation" href="#">Third Item</a></div>
+                        </div>
+                    </div>
+                    <div class="d-xl-flex align-items-xl-center" style="width: 100%;padding-top: 15px;">
+                        <div><a class="btn btn-primary border-white" data-toggle="collapse" aria-expanded="true" aria-controls="collapse-1" href="#collapse-1" role="button" style="background-color: rgb(234,67,53);">Get Virtual Account</a>
+                            <div class="collapse show"
+                                id="collapse-1">
+                                <p class="text-center">2949353502525</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8"></div>
+                <div class="col-md-4"></div>
+            </div>
+        </div>
+    </div>
