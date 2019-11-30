aa1b5be771215a5fdc63455486f6885bb77c5fa6
muhsanmaulana <muhsan.maulana@gmail.com>
2019-12-01 00:32:03 +0700

    ini resep

---
+++ application/views/resep.php
@@ -0,0 +1,38 @@
+<div class="container" style="height: 200px;"></div>
+    <div class="container text-center">
+        <h2 class="text-center">BUAT RESEPMU SENDIRI&nbsp;</h2>
+    </div>
+    <div class="container text-left d-xl-flex justify-content-xl-center align-items-xl-center" style="width: 100%;">
+        <div class="row padMar" style="width: 500px;">
+            <div class="col padMar">
+                <div class="input-group">
+                    <div class="input-group-prepend"></div><input class="form-control autocomplete" type="text" placeholder="Cari bahan">
+                    <div class="input-group-append"><button class="btn btn-warning" type="button" style="background-color: rgb(234,67,53);"><i class="fa fa-search"></i></button></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container" style="margin-top: 20px;">
+        <div class="row">
+            <div class="col-md-3" style="width: 100%;height: 320px;">
+                <div class="card" style="width: 100%;height: 100%;margin-right: 0px;"><img class="card-img-top w-100 d-block" style="width: 100%;height: 220px;" src="assets/img/persegi.png">
+                    <h4 style="font-size: 18px;margin-left: 5px;">Daging Sapi</h4>
+                    <p style="margin-left: 5px;margin-bottom: 5px;color: rgb(0,0,0);">Rp. 10.000</p><button class="btn btn-primary text-left border-white shadow-none d-lg-flex justify-content-lg-center align-items-xl-center" type="button" style="height: 35px;font-size: 14px;width: 90%;margin-left: 12px;background-color: #ea4335;">add to receipt &nbsp;<i class="icon ion-android-restaurant" style="font-size: 16px;"></i></button></div>
+            </div>
+            <div class="col-md-3" style="width: 100%;height: 320px;">
+                <div class="card" style="width: 100%;height: 100%;margin-right: 0px;"><img class="card-img-top w-100 d-block" style="width: 100%;height: 220px;" src="assets/img/persegi.png">
+                    <h4 style="font-size: 18px;margin-left: 5px;">Daging Sapi</h4>
+                    <p style="margin-left: 5px;margin-bottom: 5px;color: rgb(0,0,0);">Rp. 10.000</p><button class="btn btn-primary text-left border-white shadow-none d-lg-flex justify-content-lg-center align-items-xl-center" type="button" style="height: 35px;font-size: 14px;width: 90%;margin-left: 12px;background-color: #ea4335;">add to receipt &nbsp;<i class="icon ion-android-restaurant" style="font-size: 16px;"></i></button></div>
+            </div>
+            <div class="col-md-3" style="width: 100%;height: 320px;">
+                <div class="card" style="width: 100%;height: 100%;margin-right: 0px;"><img class="card-img-top w-100 d-block" style="width: 100%;height: 220px;" src="assets/img/persegi.png">
+                    <h4 style="font-size: 18px;margin-left: 5px;">Daging Sapi</h4>
+                    <p style="margin-left: 5px;margin-bottom: 5px;color: rgb(0,0,0);">Rp. 10.000</p><button class="btn btn-primary text-left border-white shadow-none d-lg-flex justify-content-lg-center align-items-xl-center" type="button" style="height: 35px;font-size: 14px;width: 90%;margin-left: 12px;background-color: #ea4335;">add to receipt &nbsp;<i class="icon ion-android-restaurant" style="font-size: 16px;"></i></button></div>
+            </div>
+            <div class="col-md-3" style="width: 100%;height: 320px;">
+                <div class="card" style="width: 100%;height: 100%;margin-right: 0px;"><img class="card-img-top w-100 d-block" style="width: 100%;height: 220px;" src="assets/img/persegi.png">
+                    <h4 style="font-size: 18px;margin-left: 5px;">Daging Sapi</h4>
+                    <p style="margin-left: 5px;margin-bottom: 5px;color: rgb(0,0,0);">Rp. 10.000</p><button class="btn btn-primary text-left border-white shadow-none d-lg-flex justify-content-lg-center align-items-xl-center" type="button" style="height: 35px;font-size: 14px;width: 90%;margin-left: 12px;background-color: #ea4335;">add to receipt &nbsp;<i class="icon ion-android-restaurant" style="font-size: 16px;"></i></button></div>
+            </div>
+        </div>
+    </div>
