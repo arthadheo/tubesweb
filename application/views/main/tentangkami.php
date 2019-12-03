4d99179e7823020b45f5a8631fb781d3e58d671d
Dheo artha <madza204@gmail.com>
2019-12-03 14:55:20 +0700

    nambah page tentang kami

---
+++ application/views/main/tentangkami.php
@@ -0,0 +1,59 @@
+
+<section class="section-one zm-bg-light" id="home1">
+	<div class="container">
+		<div class="zm-content-one">
+			<div class="row">
+				<div class="col-lg-8">
+                    <div>
+					    <h3 style="padding: center">TENTANG GROCERIA</h3>
+                    </div>
+					<span class="zm-subtitle zm-mb_r-2 zm-font-left zm-font-normal">Groceria adalah</span>
+                    <div class="row zm-pt-30">
+                        <div class="col-lg-12">
+                            
+                        </div>
+                    </div>
+				</div>
+				<div class="col-lg-4">
+					<h3>Alamat</h3>
+                    <p>
+                        <span class="zm-font-500 zm-font-light">Gedung Griya Legita, Lt.4</span><br> Jl. Teuku Nyak Arief, RT.7/RW.8, Simprug, Kec. Kby. Lama, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12220</p>
+                        <hr size="100%">
+					<h3>Email</h3>
+					<p class="zm-desc-e_gamify zm-mb_r-3 zm-font-left zm-font-s_16 zm-lh-23">
+                       <span class="zm-font-500">madza204@gmail.com</span></a>
+                    </p>
+                    <hr size="100%">
+					<h3>No. Telp</h3>
+					<p class="zm-desc-e_gamify zm-mb_r-3 zm-font-left zm-font-s_16 zm-lh-23">
+                       <span class="zm-font-500">0896-5478-4312</span></a>
+                    </p>
+                    <hr size="100%" >
+				</div>
+            </div>
+		</div>
+	</div>
+</section>
+<!-- /section one -->
+<!-- Javascript start -->
+<script>
+    $(document).ready(function(){
+        $('#contact').addClass('active');
+		$(".zm-nmbr").keypress(function (e) {
+			//if the letter is not digit then display error and don't type anything
+			if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+				return false;
+			}
+		});
+    })
+</script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-124227942-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-124227942-1');
+</script>
+
