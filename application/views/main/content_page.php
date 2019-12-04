b9c1f4408bfeeb5e779102f45610543aa21ab54a
Dheo artha <madza204@gmail.com>
2019-12-05 04:10:48 +0700

    benerin tentang kami

--- application/views/main/content_page.php
+++ application/views/main/content_page.php
@@ -9,11 +9,18 @@
                 $this->load->database();
             
                     echo '
-                    <div class="col-md-3" style="width: 100%;height: 330px; margin-bottom : 10px;">
+                    <div class="col-md-3 wrapper" style="width: 100%;height: 330px; margin-bottom : 10px;">
                         <div class="card" style="width: 100%;height: 100%;margin-right: 0px;">
-                        <img class="card-img-top w-100 d-block" style="width: 100%;height: 220px;" src="'.$url.'assets/img/market/' . $row->img_barang.'">
+                        <img class="card-img-top w-100 d-block alert" style="width: 100%;height: 220px;" src="'.$url.'assets/img/market/' . $row->img_barang.'">
                             <h4 style="font-size: 14px;margin-left: 5px;">'. $row->nama_barang . '</h4>
-                            <p style="margin-left: 5px;margin-bottom: 5px;color: rgb(0,0,0);">Rp.'. $row->harga . '</p><button class="btn btn-primary text-left border-white shadow-none d-lg-flex justify-content-lg-center align-items-xl-center" type="button" style="height: 35px;font-size: 14px;width: 90%;margin-left: 12px;background-color: #ea4335;">add to cart&nbsp;<i class="fa fa-cart-plus" style="font-size: 16px;"></i></button></div>
+                            <p style="margin-left: 5px;margin-bottom: 5px;color: rgb(0,0,0);">Rp.'. $row->harga . '</p>
+                            <button class="btn btn-primary text-left border-white shadow-none d-lg-flex justify-content-lg-center align-items-xl-center" 
+                            type="button" style="height: 35px;font-size: 14px;width: 90%;margin-left: 12px;background-color: #ea4335;">
+                            add to cart&nbsp;
+                            <i class="fa fa-cart-plus" style="font-size: 16px;">
+                            </i>
+                            </button>
+                            </div>
                     </div>';
                 }?>
             </div>
@@ -22,4 +29,45 @@
 
         
     </div>
-    <div class="footer-basic">
+    <div class="footer-basic"></div>
+
+<script>
+$('.add-to-cart').on('click', function () {
+        var cart = $('.shopping-cart');
+        var imgtodrag = $(this).parent('.card').find("img").eq(0);
+        if (imgtodrag) {
+            var imgclone = imgtodrag.clone()
+                .offset({
+                top: imgtodrag.offset().top,
+                left: imgtodrag.offset().left
+            })
+                .css({
+                'opacity': '0.5',
+                    'position': 'absolute',
+                    'height': '150px',
+                    'width': '150px',
+                    'z-index': '100'
+            })
+                .appendTo($('body'))
+                .animate({
+                'top': cart.offset().top + 10,
+                    'left': cart.offset().left + 10,
+                    'width': 75,
+                    'height': 75
+            }, 1000, 'easeInOutExpo');
+            
+            setTimeout(function () {
+                cart.effect("shake", {
+                    times: 2
+                }, 200);
+            }, 1500);
+
+            imgclone.animate({
+                'width': 0,
+                    'height': 0
+            }, function () {
+                $(this).detach()
+            });
+        }
+    });
+</script>
--- application/views/main/tentangkami.php
+++ application/views/main/tentangkami.php
@@ -1,5 +1,4 @@
 
-<section class="section-one zm-bg-light" id="home1">
 	<div class="container">
 		<div class="zm-content-one">
 			<div class="row">
@@ -7,62 +6,64 @@
                     <div>
 					    <h3 style="text-align: center; font-weight: bold">TENTANG GROCERIA</h3>
                     </div>
-					<span class="zm-subtitle zm-mb_r-2 zm-font-left zm-font-normal">Groceria adalah sebuah website e-commerce yang menyediakan berbagai macam bumbu dan bahan untuk memasak.</span>
+					<span class="zm-subtitle zm-mb_r-2 zm-font-left zm-font-normal"  style="padding-bottom: 10px;">Groceria adalah sebuah website e-commerce yang menyediakan berbagai macam bumbu dan bahan untuk memasak.</span>
                     <div class="row zm-pt-30">
                         <div class="col-lg-12">
 
-                        <div class="container" style="width: 100%;">
-        <div></div>
-        <div class="row">
-            <div class="col">
+        <div class="container" style="width: 100%;">
+            <div>
+                <h3 style="text-align: center; font-weight: bold; padding-bottom: 10px">Profile Developer</h3>
                 <div class="row">
-                    <div class="col d-xl-flex justify-content-xl-center"><img class="rounded-circle" src="assets/img/1.jpg" style="width: 120px;height: 120px;"></div>
+                    <div class="col">
+                        <div class="row">
+                            <div class="col d-xl-flex justify-content-xl-center"><img class="rounded-circle" src="assets/img/1.jpg" style="width: 120px;height: 120px;"></div>
+                        </div>
+                        <h6 class="text-center name" style="font-weight: bold">Fathan S. Anandika</h6>
+                        <p class="text-center title">Backend Developer</p>
+                        <p class="description" style="font-size: 12px">Bertugas sebagai backend developer. fathan membuat database, sampai semua yang berhubungan dengan backend </p>
+                    </div>
+                    <div class="col">
+                        <div class="row">
+                            <div class="col d-xl-flex justify-content-xl-center"><img class="rounded-circle" src="assets/img/1.jpg" style="width: 120px;height: 120px;"></div>
+                        </div>
+                        <h6 class="text-center name" style="font-weight: bold">Ariq Rafly</h6>
+                        <p class="text-center title">Backend Developer</p>
+                        <p class="description" style="font-size: 12px">Bertugas sebagai backend developer. Pekerjaan Ariq sama seperti fathan, membuat database, sampai semua yang berhubungan dengan backend</p>
+                    </div>
+                    <div class="col">
+                        <div class="row">
+                            <div class="col d-xl-flex justify-content-xl-center"><img class="rounded-circle" src="assets/img/1.jpg" style="width: 120px;height: 120px;"></div>
+                        </div>
+                        <h6 class="text-center name" style="font-weight: bold">Muh. M. Maulana</h6>
+                        <p class="text-center title">Frontend Developer</p>
+                        <p class="description" style="font-size: 12px">Bertugas sebagai frontend developer. Lana membuat berbagai macam layout page serta mengintegrasikan ke database yang ada</p>
+                    </div>
+                    <div class="col">
+                        <div class="row">
+                            <div class="col d-xl-flex justify-content-xl-center"><img class="rounded-circle" src="assets/img/1.jpg" style="width: 120px;height: 120px;"></div>
+                        </div>
+                        <h6 class="text-center name" style="font-weight: bold">Dheo Artha</h6>
+                        <p class="text-center title">Frontend Developer</p>
+                        <p class="description" style="font-size: 12px">Bertugas sebagai frontend developer. Dheo membuat berbagai macam layout page serta mengintegrasikan ke database yang ada</p>
+                    </div>
                 </div>
-                <h6 class="text-center name">Fathan Satria Anandika</h6>
-                <p class="text-center title">Musician</p>
-                <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
-            </div>
-            <div class="col">
-                <div class="row">
-                    <div class="col d-xl-flex justify-content-xl-center"><img class="rounded-circle" src="assets/img/1.jpg" style="width: 120px;height: 120px;"></div>
-                </div>
-                <h6 class="text-center name">Dheo Arta</h6>
-                <p class="text-center title">Musician</p>
-                <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
-            </div>
-            <div class="col">
-                <div class="row">
-                    <div class="col d-xl-flex justify-content-xl-center"><img class="rounded-circle" src="assets/img/1.jpg" style="width: 120px;height: 120px;"></div>
-                </div>
-                <h6 class="text-center name">Muh. Muhsan Maulana</h6>
-                <p class="text-center title">Musician</p>
-                <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
-            </div>
-            <div class="col">
-                <div class="row">
-                    <div class="col d-xl-flex justify-content-xl-center"><img class="rounded-circle" src="assets/img/1.jpg" style="width: 120px;height: 120px;"></div>
-                </div>
-                <h6 class="text-center name">Muh. Ariq Rafly</h6>
-                <p class="text-center title">Musician</p>
-                <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
             </div>
         </div>
-    </div>
                             
                         </div>
                     </div>
 				</div>
 				<div class="col-lg-4">
-					<h3>Alamat</h3>
+					<h5 style="font-weight: bold">Alamat</h5>
                     <p>
                         <span class="zm-font-500 zm-font-light">Gedung Griya Legita, Lt.4</span><br> Jl. Teuku Nyak Arief, RT.7/RW.8, Simprug, Kec. Kby. Lama, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12220</p>
                         <hr size="100%">
-					<h3>Email</h3>
+					<h5 style="font-weight:bold">Email</h5>
 					<p class="zm-desc-e_gamify zm-mb_r-3 zm-font-left zm-font-s_16 zm-lh-23">
                        <span class="zm-font-500">madza204@gmail.com</span></a>
                     </p>
                     <hr size="100%">
-					<h3>No. Telp</h3>
+					<h5 style="font-weight: bold">No. Telp</h5>
 					<p class="zm-desc-e_gamify zm-mb_r-3 zm-font-left zm-font-s_16 zm-lh-23">
                        <span class="zm-font-500">0896-5478-4312</span></a>
                     </p>
@@ -71,27 +72,5 @@
             </div>
 		</div>
 	</div>
-</section>
 <!-- /section one -->
 <!-- Javascript start -->
-<script>
-    $(document).ready(function(){
-        $('#contact').addClass('active');
-		$(".zm-nmbr").keypress(function (e) {
-			//if the letter is not digit then display error and don't type anything
-			if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
-				return false;
-			}
-		});
-    })
-</script>
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-124227942-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-124227942-1');
-</script>
-
