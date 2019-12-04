680e104ba47b4852c7addb62cf04b7b9e9bef9e6
Fathan Satria Anandika <fathansatriaanandika@gmail.com>
2019-12-04 15:26:36 +0700

    error fixing

--- application/controllers/Cart.php
+++ application/controllers/Cart.php
@@ -7,7 +7,7 @@ class Cart extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        $this->load->model("Barang_model");
+        $this->load->model("Cart_model");
     }
 
     public function index()
@@ -15,8 +15,8 @@ class Cart extends CI_Controller
         if(isset($_SESSION['email'])){
 
             if($_SESSION['email'] != ''){
-                $barang=$this->Barang_model;
-                $data['list_cart'] = $barang->getAllCart($_SESSION['email']);
+                $cart=$this->Cart_model;
+                $data['list_cart'] = $cart->getAllCart($_SESSION['email']);
                 $this->load->view("templates/header");
                 $this->load->view("main/mycart", $data);
                 $this->load->view("templates/footer");
@@ -30,38 +30,5 @@ class Cart extends CI_Controller
         }
 
     }
-
-
-    public function SignIn()
-    {
-        $customer = $this->Customer_model;
-        $validation = $this->form_validation;
-        $validation->set_rules($customer->signIn_rules());
-
-        if ($validation->run() == false) {
-            $this->load->view("templates/header");
-            $this->load->view("main/authorization");
-            $this->load->view("main/form_signin");
-            $this->load->view("templates/footer");
-        }
-        else{
-
-            $data = $customer->login();
-            $this->session->set_flashdata('success', 'Berhasil disimpan');
-            redirect();
-
-        }
-    }
-
-    public function SignOut(){
-
-        $_SESSION['fullname'] = '';
-        $_SESSION['email'] = '';
-        $_SESSION['alamat'] = '';
-        $_SESSION['phone'] = '';
-
-        redirect('Authorization');
-    }
-
 
 }
