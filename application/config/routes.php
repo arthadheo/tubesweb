d9f2f3b4fc7ad7224ba86b11ae9c171e3e73e002
Fathan Satria Anandika <fathansatriaanandika@gmail.com>
2019-12-05 04:26:10 +0700

    checkout

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,7 +50,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'mainpage';
-$route['registration'] = 'Registration';
 $route['belanja'] = 'mainpage/belanja';
 $route['mycart'] = 'mainpage/mycart';
 $route['resep'] = 'mainpage/resep';
--- application/controllers/Checkout.php
+++ application/controllers/Checkout.php
@@ -2,13 +2,45 @@
 class Checkout extends CI_Controller{
 	
 	function __construct(){
-		parent::__construct();
+        parent::__construct();
+        $this->load->model("Cart_model");
+
 	}
 
 	function index(){
-		$this->load->view("templates/header");
-        $this->load->view("main/checkout");
-        $this->load->view("templates/footer");
+
+        $cart=$this->Cart_model;
+        $data['total'] = $cart->getTotal();
+        $this->load->view("templates/header");
+        $this->load->view("main/checkout", $data);
+        $this->load->view("templates/footer", $data);
+
+    }
+
+    function createInvoice($ongkir, $total){
+        
+        $virtual = rand();
+
+        $cart=$this->Cart_model;
+        $cart->clearCart();
+
+        echo ('<div class="card text-center">
+        <div class="card-header">
+          Segera Lakukan Pembayaran
+        </div>
+        <div style = "background-color:#f66464"; class="card-body">
+          <h3 style = "margin-bottom:20px"; class="card-title">Invoice</h3>
+          <h4 style = "margin-bottom:20px";>Rp '.number_format($total).'</h4>
+          <a style = "color:#ffffff;" class="btn btn-primary">'. $virtual.'</a>
+        </div>
+        <div class="card-footer text-muted">
+          Terima Kasih 
+        </div>
+      </div>');
+
+
+
+
     }
     
 }
