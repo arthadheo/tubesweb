f44eac143cea285675e46be5eedf1ad7289de2fe
muhsanmaulana <muhsan.maulana@gmail.com>
2019-12-04 20:45:08 +0700

    detail barang

---
+++ application/views/main/detailbrang.php
@@ -0,0 +1,16 @@
+<div class="container" style="height: 40px;"></div>
+    <div class="container">
+        <h3 style="height: 40px;">Dagin Sapi</h3>
+    </div>
+    <div style="width: 100%;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6"><img src="assets/img/persegi.png" style="width: 220px;"></div>
+                <div class="col-md-6">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi u<br></p>
+                    <h6>Rp. 12000</h6>
+                    <h6 class="d-inline-block" style="width: 35px;">400</h6>
+                    <h6 class="d-inline-block" style="width: 45%;">Gram</h6><button class="btn btn-primary text-left border-white shadow-none d-lg-flex justify-content-lg-center align-items-xl-center" type="button" style="height: 35px;font-size: 14px;width: 200px;margin-left: 0px;background-color: #ea4335;margin-top: 20px;">add to cart&nbsp;<i class="fa fa-cart-plus" style="font-size: 16px;"></i></button></div>
+            </div>
+        </div>
+    </div>
